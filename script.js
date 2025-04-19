eacfd6ae649799a5f2c2900650f5024e2a9ec42a
175Shadow571 <omerfarukmusab2003@gmail.com>
2025-04-19 13:06:43 +0300

    zum devre dışı

--- script.js
+++ script.js
@@ -5,8 +5,95 @@ let nokta = document.querySelector("#nokta")
 let inputSonuc = document.querySelector("#inputSonuc")
 
 
-function noktaVarmi(){
-    let tersVeri = input.value.split("").reverse().join("")
+
+
+
+
+
+
+buttons.forEach(function(button){
+    button.addEventListener("click",function(){
+        input.value += button.textContent
+        NoktaVarmi(input.value)
+    })
+})
+
+
+buttonsResim.forEach(function(veri) {
+    veri.addEventListener("click", function(tiklanan){
+        let button = tiklanan.target
+       if(button.id == "temizle"){
+        input.value = ""
+       }else if (button.id == "kalan" && SayiVarmi(input.value)){
+        input.value += "%"
+       }else if (button.id == "sil"){
+        input.value = input.value.slice(0,(input.value.length - 1))
+       }else if(button.id == "hesapla"){
+        Hesapla(input.value);
+       }else if(button.id == "bol" && SayiVarmi(input.value)){
+        input.value += "÷"
+       }else if(button.id == "carp" && SayiVarmi(input.value)){
+        input.value += "X"
+       }else if(button.id == "cikart" && SayiVarmi(input.value)){
+        input.value += "－"
+       }else if(button.id == "topla" && SayiVarmi(input.value)){
+        input.value += "＋"
+    }
+       NoktaVarmi(input.value);
+    })
+})
+
+
+function Hesapla(yazi){
+
+    const sonuc = yazi.split("").reduce((sonuc,veri) =>{
+     if(veri == "＋"){
+            let [sayi, sayi2]  = yazi.split("＋").slice()
+            return  parseFloat(sayi) + parseFloat(sayi2)
+        }else if(veri == "－"){
+            let [sayi, sayi2]  = yazi.split("－").slice()
+            return  parseFloat(sayi) - parseFloat(sayi2)
+        }else if(veri == "X"){
+            let [sayi, sayi2]  = yazi.split("X").slice()
+            return  parseFloat(sayi) * parseFloat(sayi2)
+        }else if(veri == "÷"){
+            let [sayi, sayi2]  = yazi.split("÷").slice()
+            return  parseFloat(sayi) / parseFloat(sayi2)
+        }else if(veri == "%"){
+            let [sayi, sayi2]  = yazi.split("%").slice()
+            return  parseFloat(sayi) % parseFloat(sayi2)
+        }
+        return sonuc
+        
+    },0)
+    input.value = sonuc
+}
+
+
+function Topla(sayi1,sayi2){
+
+}
+function Cikar(sayi1,sayi2){
+
+}
+function Carp(sayi1, sayi2){
+
+}
+function Bol(sayi1,sayi2){
+
+}
+function Kalan(sayi1, sayi2){
+
+}
+
+
+
+
+
+
+
+function NoktaVarmi(yazi){
+    let tersVeri = yazi.split("").reverse().join("")
     let ayrilmisVeri = tersVeri.split(/[%\÷\X\－\＋]/)[0]
     
     if(ayrilmisVeri.includes(".") || ayrilmisVeri.length == 0){
@@ -16,11 +103,8 @@ function noktaVarmi(){
     }
 }
 
-
-
-
-function sayiVarmi(){
-    let tersVeri = input.value.split("").reverse().join("")
+function SayiVarmi(yazi){
+    let tersVeri = yazi.split("").reverse().join("")
     let ayrilmisVeri = tersVeri.split(/[%\÷\X\－\＋]/)[0]
     
     if(/[0-9]/.test(ayrilmisVeri)){
@@ -28,80 +112,4 @@ function sayiVarmi(){
     }else{
         return false
     }
-}
-
-
-
-buttons.forEach(function(button){
-    button.addEventListener("click",function(){
-        input.value += button.textContent
-        noktaVarmi()
-    })
-})
-
-let sayi = 0;
-let arrr = [];
-
-buttonsResim.forEach(function(veri) {
-    veri.addEventListener("click", function(tiklanan){
-        let button = tiklanan.target
-       if(button.id == "temizle"){
-        input.value = ""
-       }else if (button.id == "kalan"){
-        if(sayiVarmi()){input.value += "%"}
-        
-       }else if (button.id == "sil"){
-        input.value = input.value.slice(0,(input.value.length - 1))
-       }else if(button.id == "hesapla"){
-        hesapla();
-       }else if(button.id == "bol"){
-        if(sayiVarmi()){input.value += "÷"}
-        
-       }else if(button.id == "carp"){
-        if(sayiVarmi()){input.value += "X"}
-        
-       }else if(button.id == "cikart"){
-        if(sayiVarmi()){input.value += "－"}
-        
-       }else if(button.id == "topla"){
-        if(sayiVarmi()){input.value += "＋"}
-        
-        arrr.push(input.value.slice(sayi,(input.value.length - 1)))
-        sayi = input.value.length
-        console.log(arrr);
-        
-       }
-       noktaVarmi();
-
-    })
-})
-
-// %  ÷    let input = document.querySelector("#input")
-
-function hesapla(){
-
-    arrr.reduce((topam, veri) =>{
-        console.log(veri);
-        
-    },0)
-
-    const sonuc = input.value.split("").reduce((sonuc,veri) =>{
-
-
-        
-
-
-        if(veri == "＋"){
-            let [sayi, sayi2]  = input.value.split("＋").slice()
-           
-            return  parseInt(sayi) + parseInt(sayi2)
-        }else if(veri == "－"){
-
-        }else if(veri == "X"){
-
-        }
-        return sonuc
-        
-    },0)
-    input.value = sonuc
 }
